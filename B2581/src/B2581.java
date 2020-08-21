6e6917ecfc3fe30cc5aec329a2b46c92d64921e7
deriko <deriko@naver.com>
2020-08-21 22:16:29 +0900

    prime number

---
+++ B2581/src/B2581.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class B2581 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+
+		int m = s.nextInt();
+		int n = s.nextInt();
+		int min = 0;
+		int sum = 0;
+		int arr[] = new int[n-m + 1];
+		int idx = 0;
+
+		for (int i = m; i <= n; i++) {
+			int socnt = 0;
+
+			for (int j = 1; j <= i; j++) {
+
+				if (i % j == 0) {
+					socnt++;
+				}
+
+			}
+			if (socnt == 2) {
+				arr[idx] = i;
+				sum += i;
+				idx++;
+			}
+
+		}
+		if (sum==0&&arr[0]==0) System.out.println(-1);
+		else {
+			System.out.println(sum);
+			System.out.println(arr[0]);
+		}
+
+	}
+
+}

780578ccc6888301a81493b2a036d7a856ecac53
gotaek <64013443+gotaek@users.noreply.github.com>
2020-09-15 21:10:24 +0900

    N and M 3

---
+++ B15651/src/B15651.java
@@ -0,0 +1,40 @@
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+import java.io.*;
+
+public class B15651 {
+	// main과 dfs 함수에서 공통적으로 사용해야 하므로 이곳에 배열을 생성
+	public static boolean checker[];
+	public static int arr[];
+	public static StringBuilder sb=new StringBuilder();
+	
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		arr = new int[M];
+		dfs(N, M, 0);
+		System.out.print(sb);
+	}
+
+	static void dfs(int n, int m, int depth) {
+		if (m == depth) {
+			
+			for (int i : arr) {
+				sb.append(i+" ");
+			}
+			sb.append("\n");
+			return;
+		}
+
+		for (int i = 1; i <= n; i++) {
+			arr[depth] = i;
+			dfs(n, m, depth + 1);
+
+		}
+	}
+}

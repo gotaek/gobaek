672404404e4ddf3d7a6eea48e0eb9cfad9d319ac
gotaek <64013443+gotaek@users.noreply.github.com>
2020-12-08 10:28:14 +0900

    Min Heap

--- B11279/src/B11279.java
+++ B11279/src/B11279.java
@@ -1,28 +1,61 @@
-import java.io.*;
-import java.util.Collections;
-import java.util.PriorityQueue;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+ 
 public class B11279 {
-
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		int n = Integer.parseInt(br.readLine());
-		PriorityQueue<Integer> pq = new PriorityQueue(Collections.reverseOrder());
-
-		for (int i = 0; i < n; i++) {
-			int x = Integer.parseInt(br.readLine());
-			if (x == 0) {
-				if (pq.isEmpty()) {
-					System.out.println(0);
-				} else {
-					System.out.println(pq.poll());
-				}
-
-			} else {
-				pq.offer(x);
-			}
-		}
-	}
+    public static int [] heap = new int[100001];
+    public static int size = 0;
+    public static StringBuilder sb = new StringBuilder();
+ 
+    public static void swap(int x, int y) {
+        int temp = heap[x];
+        heap[x] = heap[y];
+        heap[y] = temp;
+    }
+    
+    public static void push(int x) {
+    	heap[++size] = x;
+        for(int i=size; i>1; i/=2) {
+            if(heap[i/2] < heap[i]) {
+                swap(i/2, i);
+            } else {
+                break;
+            }
+        }
+    }
+ 
+    public static void pop() {
+        sb.append(heap[1]+"\n");
+        heap[1] = heap[size];
+        heap[size--] = 0;
+        for(int i=1; i*2<=size;) { 
+            if(heap[i] > heap[i*2] && heap[i] > heap[i*2+1]) {
+                break;
+            }else if(heap[i*2] > heap[i*2+1]) {
+                swap(i, i*2);
+                i = i*2;
+            } else {
+                swap(i, i*2+1);
+                i = i*2+1;
+            }
+        }
+    }
+    
+    public static void main(String[] args) throws IOException{
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(reader.readLine());
+        for(int i=0; i<n; i++) {
+            int k = Integer.parseInt(reader.readLine());
+            if(k==0) {
+                if(size == 0) {
+                    sb.append(0+"\n");
+                } else {
+                    pop();
+                }
+            } else {
+                push(k);
+            }
+        }
+        System.out.print(sb.toString());
+    }
 }
---
+++ B1927/src/B1927.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.PriorityQueue;
+public class B1927 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		PriorityQueue<Integer> pq=new PriorityQueue();
+		
+		int n=Integer.parseInt(br.readLine());
+		
+		for(int i=0;i<n;i++) {
+			int x=Integer.parseInt(br.readLine());
+			if(x==0) {
+				if(pq.isEmpty()) {
+					System.out.println(0);
+				}else {
+					System.out.println(pq.poll());
+				}
+			}else {
+				pq.offer(x);
+			}
+		}
+	}
+
+}

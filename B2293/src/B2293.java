d20fbf54e2b5829202a3a76d2fcac290f922d45d
gotaek <64013443+gotaek@users.noreply.github.com>
2020-12-09 15:45:50 +0900

    coin 1

---
+++ B2293/src/B2293.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class B2293 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+
+		int dp[] = new int[k + 1];
+		dp[0] = 1;
+		for (int i = 0; i < n; i++) {
+			int coin = Integer.parseInt(br.readLine());
+			for (int j = 1; j <= k; j++) {
+				if (j >= coin) {
+					dp[j] = dp[j] + dp[j - coin];
+				}
+			}
+		}
+		System.out.println(dp[k]);
+	}
+
+}
